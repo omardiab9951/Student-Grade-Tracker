cf1fcdb6515adf89e59d7318ae34c7d68f5cd13a
Omar Diab <omarkamaldiab9951@gmail.com>
2026-03-19 22:56:26 +0200

    Add files via upload

---
+++ Code/student_grade_tracker.py
@@ -0,0 +1,10 @@
+def show_menu():
+    print("--------------------------------")
+    print("     Student Grade Tracker Program     ")
+    print("1. Add Student")
+    print("2. View All Students")
+    print("3. Check Pass OR Fail")
+    print("4. Exit")
+    print("--------------------------------")
+  
+show_menu()
